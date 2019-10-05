9634c5aa53ec0235bc8b4bdde0a941fbba7a3eb0
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-05 03:21:57 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,5 +2,9 @@ first_number = 10
 second_number = 20
 sum = first_number + second_number
 difference = first_number - second_number
+<<<<<<< HEAD
 product = first_number * second_number
 quotient = first_number / second_number
+=======
+product = first_number * second_number
+>>>>>>> 332d1532e878a22d89b91f3b55967c6ade1b0008
